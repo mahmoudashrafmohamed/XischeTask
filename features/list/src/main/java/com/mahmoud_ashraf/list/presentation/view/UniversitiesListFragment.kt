fe926d65d515d0c46aa41e28bf3fdf2ebf56a91d
Mahmoud Ashraf <ashraaf.mahmoud@gmail.com>
2024-05-04 16:38:52 +0300

    Finish bind UniversitiesListFragment data

--- features/list/src/main/java/com/mahmoud_ashraf/list/presentation/view/UniversitiesListFragment.kt
+++ features/list/src/main/java/com/mahmoud_ashraf/list/presentation/view/UniversitiesListFragment.kt
@@ -6,7 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import com.mahmoud_ashraf.list.R
+import com.mahmoud_ashraf.list.data.remote.model.GetUniversitiesResponseItem
+import com.mahmoud_ashraf.list.databinding.FragmentUniversitiesListBinding
 import com.mahmoud_ashraf.list.presentation.viewmodel.UniversitiesListViewModel
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
@@ -20,21 +21,36 @@ class UniversitiesListFragment :DaggerFragment() {
     ViewModelProvider(this, viewModelProviderFactory)[UniversitiesListViewModel::class.java]
   }
 
+  private var _binding: FragmentUniversitiesListBinding? = null
+  private val binding get() = _binding!!
+
+  private val universitiesListAdapter by lazy {  UniversitiesListAdapter() }
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?,
-  ): View? {
-    return inflater.inflate(R.layout.fragment_universities_list, container, false)
+  ): View {
+    _binding = FragmentUniversitiesListBinding.inflate(inflater, container, false)
+    return binding.root
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    viewModel.universitiesList.observe(viewLifecycleOwner){
-      Log.e("current list = ",""+it.toString())
+    binding.rvUniversities.adapter = universitiesListAdapter
+      viewModel.universitiesList.observe(viewLifecycleOwner){
+      Log.d("current list = ",""+it.toString())
+      bindUI(it)
     }
     viewModel.getUniversities()
   }
 
+  private fun bindUI(items: List<GetUniversitiesResponseItem>) {
+    universitiesListAdapter.submitList(items)
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
 
 }
